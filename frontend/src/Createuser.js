a0b915802b45e707a8d2ef706bed921bf078f8a1
Sandeep-1405 <sandeepmaraboina14s@gmail.com>
2024-04-05 23:13:15 +0530

    component update

--- frontend/src/Createuser.js
+++ frontend/src/Createuser.js
@@ -36,7 +36,7 @@ function Createuser(){
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className="w-50 bg-white rounded p-3">
                 <form onSubmit={handleSubmit}>
-                    <h2>Add Student</h2>
+                    <h2>Add User</h2>
                     <div className="mb-2">
                         <label htmlFor="">Name</label>
                         <input type="text" placeholder="Enter your Name" className="form-control" onChange={e => setName(e.target.value)}/>
--- frontend/src/Updateuser.js
+++ frontend/src/Updateuser.js
@@ -30,7 +30,7 @@ function Updateuser(){
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className="w-50 bg-white rounded p-3">
                 <form onSubmit={handleSubmit}>
-                    <h2>Update Student</h2>
+                    <h2>Update User</h2>
                     <div className="mb-2">
                         <label htmlFor="">Name</label>
                         <input type="text" placeholder="Enter Name" className="form-control" onChange={e => setName(e.target.value)}/>
